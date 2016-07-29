288da4bff750034d4766baddfba2aff557054660
gwhite <gemma.white@lastminute.com>
2016-07-29 13:01:13 +0100

    added sotm monitor - user inputs exact performance name and results are returned

--- report.py
+++ report.py
@@ -47,7 +47,7 @@ def ls_summary():
 
 
 def sales_by_show():
-    mainList = monga.mongo_call("2016-07-01", "2016-07-29")
+    mainList = monga.mongo_call("2015-12-01", "2015-12-31")
     monga.create_docs(mainList[0], mainList[1], mainList[2], mainList[3], mainList[4], mainList[5], mainList[6], mainList[7], mainList[8], mainList[9], mainList[10], mainList[11])
 
 # ls_summary()
@@ -76,37 +76,49 @@ def quick_spot():
     print("Total Commission:" + "£" + (str(mainList[6])))
 
 
-def spot_check():
-    try:
-        year1 = str(input('Enter a year: (START) (' + str(date.today().year) + '): '))
-    except:
-        year1 = str(date.today().year)
-    if year1 == '':
-        year1 = str(date.today().year)
-        print(year1)
-        month1 = input('Enter a month: (START) ')
-        day1 = input('Enter a day: (START) ')
-    try:
-        year2 = str(input('Enter a year: (END) (' + str(date.today().year) + '): '))
-    except:
-        year2 = str(date.today().year)
-    if year2 == '':
-        year2 = str(date.today().year)
-        print(year2)
-        month2 = input('Enter a month: (END) ')
-        day2 = input('Enter a day: (END) ')
+# def spot_check():
+#     try:
+#         year1 = str(input('Enter a year: (START) (' + str(date.today().year) + '): '))
+#     except:
+#         year1 = str(date.today().year)
+#     if year1 == '':
+#         year1 = str(date.today().year)
+#         print(year1)
+#         month1 = input('Enter a month: (START) ')
+#         day1 = input('Enter a day: (START) ')
+#     try:
+#         year2 = str(input('Enter a year: (END) (' + str(date.today().year) + '): '))
+#     except:
+#         year2 = str(date.today().year)
+#     if year2 == '':
+#         year2 = str(date.today().year)
+#         print(year2)
+#         month2 = input('Enter a month: (END) ')
+#         day2 = input('Enter a day: (END) ')
+#
+#
+#     mainList = monga.mongo_call(startTime, endTime)
+#     print("Total GTV:" + "£" + (str(mainList[2])))
+#     print("Total Bookings:" + (str(mainList[3])))
+#     print("Total Tickets:" + (str(mainList[4])))
+#     print("Total Commission:" + "£" + (str(mainList[6])))
 
 
-    mainList = monga.mongo_call(startTime, endTime)
-    print("Total GTV:" + "£" + (str(mainList[2])))
-    print("Total Bookings:" + (str(mainList[3])))
-    print("Total Tickets:" + (str(mainList[4])))
-    print("Total Commission:" + "£" + (str(mainList[6])))
+def sotm_show():
+    mainList = monga.mongo_call("2016-07-01", "2016-07-31")
+    showD = mainList[9]
+    sotmShow = input('Enter the show (must be precise): ')
+    v = showD[sotmShow]
+    gtv = [item[0] for item in v]
+    tickets = [item[1] for item in v]
+    print("SOTM: " + str(sotmShow))
+    print("Total GTV:" + "£" + str(sum(gtv)))
+    print("Total Tickets:" + str(sum(tickets)))
 
 
 def choose_report():
     # input = raw_input
-    userChoice = int(eval(input('Choose a report: /n (1) summary (2) sales by show (3) hourly (4) spot check (5) today (6) quick spot ')))
+    userChoice = int(eval(input('Choose a report: /n (1) summary (2) sales by show (3) hourly (4) today (5) quick spot (6) SOTM ')))
     if userChoice == 1:
         ls_summary()
     if userChoice == 2:
@@ -114,9 +126,9 @@ def choose_report():
     if userChoice == 3:
         hourly_heatmap()
     if userChoice == 4:
-        spot_check()
-    if userChoice == 5:
         today_spot()
-    if userChoice == 6:
+    if userChoice == 5:
         quick_spot()
+    if userChoice == 6:
+        sotm_show()
 choose_report()
