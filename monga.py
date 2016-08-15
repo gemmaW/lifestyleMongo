08e492c59c29707088bb86ee19bfbd2b2367c60e
gwhite <gemma.white@lastminute.com>
2016-08-15 17:48:22 +0100

    'price per ticket' added
    'supplier' in myList has been changed (column heading) to venue to avoid confusion
    
    supplier group added
    performance time added - still should be simpler.
    column headings added to sales by show report...
    
    still to do: face value? and restaurant on a separate line - for finance report

--- monga.py
+++ monga.py
@@ -92,7 +92,7 @@ def mongo_call(startDate, endDate):
     total_transactions = 0
     meal_deals = 0
     total_tickets = 0
-    mylist = [['ID', 'Date', 'Performance', 'Total Price', 'Booking Fee', 'Commission Amount', 'Margin %', 'No of Tickets', 'Meal Deal', 'Restaurant Price PP', 'Discounts', 'Platform', 'Restoration Levy', 'Venue', 'User Agent', 'Referrer', 'Supplier Key', 'ENTA Reference', 'Performance Date & Time', 'Supplier Group']]
+    mylist = [['ID', 'Date', 'Performance', 'Total Price', 'Booking Fee', 'Commission Amount', 'Margin %', 'No of Tickets', 'Meal Deal', 'Restaurant Price PP', 'Discounts', 'Platform', 'Restoration Levy', 'Venue', 'User Agent', 'Referrer', 'Supplier Key', 'ENTA Reference', 'Performance Date & Time', 'Supplier Group', 'Price Per Ticket']]
     filename = 'sales_' + my_start_str[:10] + '_' + my_end_str[:10] + '.csv'
 
 
@@ -204,6 +204,16 @@ def mongo_call(startDate, endDate):
         except:
             officeCode = ""
 
+        try:
+            pricePerTkt = j["displayPrices"]["pricePerTicket"]
+        except:
+            pricePerTkt = ""
+
+        # try:
+        #     restaurantName = j["displayPrices"]["pricePerTicket"]
+        # except:
+        #     pricePerTkt = ""
+
         perfDateG = perfYear + "-" + perfMonth + "-" + perfDay + " " + perfHour + ":" + perfMin
 
         times = (str(x.isoLastModifiedDateTime[total_transactions])).split(":")
@@ -221,7 +231,7 @@ def mongo_call(startDate, endDate):
         timeString = londonTime.strftime('%Y-%m-%d %H:%M')
 
         # print x.orderId[total_transactions], x.isoLastModifiedDateTime[total_transactions].strftime("%Y-%m-%d %H:%M"), j["performance"]["name"], float(j["displayPrices"]["grandTotal"]), int(j["tickets"][0]["quantity"]), md, restaurantPPP, promoMessage, j["financeData"]["productSourceSystem"]
-        mylist.append([str(x.orderId[total_transactions]), timeString, j["performance"]["name"], float(j["displayPrices"]["grandTotal"]), bookingFee, commission, margin, int(j["tickets"][0]["quantity"]), md, restaurantPPP, promoMessage, j["financeData"]["productSourceSystem"], restorationLevy, supplier, agent, referer, supplierKey, entaRef, perfDateG, officeCode])
+        mylist.append([str(x.orderId[total_transactions]), timeString, j["performance"]["name"], float(j["displayPrices"]["grandTotal"]), bookingFee, commission, margin, int(j["tickets"][0]["quantity"]), md, restaurantPPP, promoMessage, j["financeData"]["productSourceSystem"], restorationLevy, supplier, agent, referer, supplierKey, entaRef, perfDateG, officeCode, pricePerTkt])
         # print j['displayPrices']
         # print float(j["displayPrices"]["grandTotal"])
         total_transactions += 1
